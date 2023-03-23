7d5da78cabe43e0faf8900c2abc19a21ef1ed91c
Abel Adisu <abeladisu73@gmail.com>
2023-03-22 23:26:48 -0700

    comit

---
+++ bf/rotr.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * _rotr - rotates the stack to the bottom.
+ * @stack: double pointer to header (top) of the stack.
+ * @line_number: counter for line number of the file.
+ *
+ * Return: void.
+ */
+void _rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp, *last;
+
+	(void) line_number;
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+
+	last = *stack;
+	while (last->next != NULL)
+		last = last->next;
+
+	temp = *stack;
+	*stack = last;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
+	(*stack)->next = temp;
+	temp->prev = *stack;
+}
