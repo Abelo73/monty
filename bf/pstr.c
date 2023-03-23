780048587db1c7f003f63bd53ca49e2fda776ba2
Abel Adisu <abeladisu73@gmail.com>
2023-03-22 23:22:13 -0700

    pstr

---
+++ bf/pstr.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * _pstr - prints the string starting at the top of the stack.
+ * @stack: double pointer to header (top) of the stack.
+ * @line_number: counter for line number of the file.
+ *
+ * Return: void.
+ */
+void _pstr(stack_t **stack, unsigned int line_number)
+{
+	int n;
+	stack_t *temp = *stack;
+
+	(void) line_number;
+	if (*stack == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	while (temp != NULL)
+	{
+		n = temp->n;
+		if (!(n >= 1 && n <= 127) || n == 0)
+			break;
+		printf("%c", n);
+		temp = temp->next;
+	}
+	printf("\n");
+}
